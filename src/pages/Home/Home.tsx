07740d7d72eaae612db94de514a3b84f3e1c57cf
alxdfm <alexandre_fmachado@hotmail.com>
2022-12-15 00:59:24 -0300

    Update root styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,80 +8,87 @@ import { PostType } from "../../components/Posts/Posts";
 import Profile from "../../components/Profile";
 import Technologies from "../../components/Technologies";
 import { GET_ALL_POSTS } from "../../graphql/queries/get-all-posts";
-import { ScreenContainer, SideContent, MainContent } from "./styles";
+import {
+  ScreenContainer,
+  SideContent,
+  MainContent,
+  FullContainer,
+} from "./styles";
 
 const Home = () => {
-  const { data } = useQuery<{ posts: PostType[]}>(GET_ALL_POSTS);
+  const { data } = useQuery<{ posts: PostType[] }>(GET_ALL_POSTS);
 
   if (!data) {
     return <></>;
   }
 
   return (
-    <ScreenContainer>
-      <SideContent>
-        <Profile
-          name={"Alexandre Machado"}
-          description={"Full Stack Developer"}
-          url={"https://www.github.com/alxdfm.png"}
-        />
-        <Links {...linksDataDefault} />
-        <Technologies
-          techTags={[
-            "Javascript",
-            "Typescript",
-            "Java",
-            "ReactJS",
-            "NodeJS",
-            "Git",
-            "HTML",
-            "CSS",
-            "Styled-components",
-            "NestJs",
-            "GraphQL",
-            "Rest",
-          ]}
-        />
-        <Experiences
-          title="Experiências"
-          experiences={[
-            {
-              company: "Descomplica",
-              start: 2022,
-              end: "Atualmente",
-              role: "Desenvolvedor Fullstack",
-            },
-            {
-              company: "AEL Sistemas",
-              start: 2019,
-              end: 2022,
-              role: "Técnico em Eletrônica",
-            },
-          ]}
-        />
-        <Experiences
-          title="Educação"
-          experiences={[
-            {
-              company: "Faculdade UniRitter",
-              start: 2020,
-              end: 2022,
-              role: "Análise e Desenvolvimento de Sistemas",
-            },
-            {
-              company: "Escola Técnica Parobé",
-              start: 2017,
-              end: 2019,
-              role: "Curso Técnico em Eletrônica",
-            },
-          ]}
-        />
-      </SideContent>
-      <MainContent>
-        <MyProjects />
-        <Posts posts={data.posts}/>
-      </MainContent>
-    </ScreenContainer>
+    <FullContainer>
+      <ScreenContainer>
+        <SideContent>
+          <Profile
+            name={"Alexandre Machado"}
+            description={"Full Stack Developer"}
+            url={"https://www.github.com/alxdfm.png"}
+          />
+          <Links {...linksDataDefault} />
+          <Technologies
+            techTags={[
+              "Javascript",
+              "Typescript",
+              "Java",
+              "ReactJS",
+              "NodeJS",
+              "Git",
+              "HTML",
+              "CSS",
+              "Styled-components",
+              "NestJs",
+              "GraphQL",
+              "Rest",
+            ]}
+          />
+          <Experiences
+            title="Experiências"
+            experiences={[
+              {
+                company: "Descomplica",
+                start: 2022,
+                end: "Atualmente",
+                role: "Desenvolvedor Fullstack",
+              },
+              {
+                company: "AEL Sistemas",
+                start: 2019,
+                end: 2022,
+                role: "Técnico em Eletrônica",
+              },
+            ]}
+          />
+          <Experiences
+            title="Educação"
+            experiences={[
+              {
+                company: "Faculdade UniRitter",
+                start: 2020,
+                end: 2022,
+                role: "Análise e Desenvolvimento de Sistemas",
+              },
+              {
+                company: "Escola Técnica Parobé",
+                start: 2017,
+                end: 2019,
+                role: "Curso Técnico em Eletrônica",
+              },
+            ]}
+          />
+        </SideContent>
+        <MainContent>
+          <MyProjects />
+          <Posts posts={data.posts} />
+        </MainContent>
+      </ScreenContainer>
+    </FullContainer>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,12 +5,14 @@ export const ScreenContainer = styled.section`
   flex-direction: row;
   width: 980px;
   gap: 58px;
+  margin: 8px;
 `;
 
 export const SideContent = styled.div`
   display: flex;
   flex-direction: column;
   width: 300px;
+  margin-bottom: 20px;
   gap: 12px;
 `;
 
@@ -19,4 +21,12 @@ export const MainContent = styled.div`
   flex-direction: column;
   gap: 12px;
   width: 580px;
+`;
+
+export const FullContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: #000;
 `;
